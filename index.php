38af362f18e99aa228c1a2da3fa3627ec43a2dcc
shinji <shinji.sueyoshi.0630@gmail.com>
2014-12-06 01:08:35 +0900

    init

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<h1>hello world</h1>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Portfolio</title>
+  <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.9.0/build/reset/reset-min.css">
+  <link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+<style>
+body{
+  font-family: 'Slabo 27px', serif;
+}
+</style>
+</head>
+<body>
+  <h1>hello world</h1>
+</body>
+</html>
